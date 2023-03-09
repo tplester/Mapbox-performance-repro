f034a4e83b1cf192615009186365b78869dfa4d1
Tim Plester <tplester@deloitte.com.au>
2023-03-09 17:21:18 +1100

    Added reproduction case for stuttering issue

---
+++ Assets/ZoomManager.cs
@@ -0,0 +1,30 @@
+﻿using DG.Tweening;
+using Mapbox.Unity.Map;
+using System.Collections;
+using UnityEngine;
+using static UnityEditor.PlayerSettings;
+
+namespace Assets
+{
+	public class ZoomManager : MonoBehaviour
+	{
+		[SerializeField]
+		AbstractMap _map;
+
+		[SerializeField]
+		float zoomOutLevel = 4;
+
+		[SerializeField]
+		float zoomInLevel = 12;
+
+		public void ZoomIn()
+		{
+			DOTween.To(() => _map.Zoom, x => _map.UpdateMap(x), zoomInLevel, 3f);
+		}
+
+		public void ZoomOut()
+		{
+			DOTween.To(() => _map.Zoom, x => _map.UpdateMap(x), zoomOutLevel, 3f);
+		}
+	}
+}
